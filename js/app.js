32a40ddcc815e7d5d4600dce66dded00501a171a
mitchgemma <mitchgemma@gmail.com>
2022-02-20 15:22:56 -0500

    Added alternating turns

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 const container = document.getElementById ('container')
+let gameBoard = [
+                ["_", "_", "_", "_", "_", "_", "_",],
+                ["_", "_", "_", "_", "_", "_", "_",],
+                ["_", "_", "_", "_", "_", "_", "_",],
+                ["_", "_", "_", "_", "_", "_", "_",],
+                ["_", "_", "_", "_", "_", "_", "_",],
+                ["_", "_", "_", "_", "_", "_", "_",],
+];
+let player = 1
+let lastMove = "red"
+let currentBlack = "black"
+let currentRed = "red"
 
 
 // create a function that will generate all of the individual game tiles
@@ -11,6 +23,35 @@ const createBoard = () => {
         gameTile.className = "tile"
         //append these squares to the container
         container.appendChild(gameTile)
+        // want to add a way to make tiles clickable
+    
+        gameTile.addEventListener('click', click =>{
+            // alternate which color a tile will turn
+            gameTile.style.backgroundColor = lastMove
+            //if last color was red, make the next one black
+            if (lastMove === "red") {
+                lastMove = "black"
+            // if last color was black, make the next one red
+            } else if (lastMove === "black"){
+                lastMove = "red"
+            }
+            // console.log("tile was clicked")
+            if (gameTile.style.backgroundColor !== "red" || "black"){
+                
+            }
+        })
     }
 }
-createBoard()
+// create a function to alternate between 2 colors
+
+
+
+
+
+
+console.log('this will give us our board array: ', gameBoard)
+
+//once the DOM loads, this will run
+document.addEventListener('DOMContentLoaded', () => {
+    createBoard()
+})
